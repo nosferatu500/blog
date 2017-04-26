174b10a445296e5ed56fb12041c5fffa62bd4322
Vitaly Shvetsov <nosferatu2995@mail.ru>
2017-04-26 14:07:20 +0700

    Pre-Complete LatestPosts

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,7 +13,7 @@ module.exports = {
   extends: 'standard',
   // required to lint *.vue files
   plugins: [
-    'html'
+    'html', 'compat'
   ],
   // add your custom rules here
   'rules': {
@@ -22,6 +22,8 @@ module.exports = {
     // allow async-await
     'generator-star-spacing': 0,
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    
+    "compat/compat": 2
   }
 }
--- src/components/LatestsPosts.vue
+++ src/components/LatestsPosts.vue
@@ -1,8 +1,55 @@
 <template lang="pug">
   section
-    h1 Latest
+    .container
+      h1.s_title latest posts 
+      .columns
+        .column.is-3
+          .s_item
+            img(src="../../src/assets/img/latestposts/banner.png", alt="")
+            h1 Title
+            p  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident accusantium, ad sunt dignissimos sit nisi harum assumenda repellendus ipsam dolore sequi voluptas delectus placeat optio tempore quod voluptatibus est dolorem!
+        .column.is-3
+          .s_item
+            img(src="../../src/assets/img/latestposts/banner.png", alt="")
+            h1 Title
+            p  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident accusantium, ad sunt dignissimos sit nisi harum assumenda repellendus ipsam dolore sequi voluptas delectus placeat optio tempore quod voluptatibus est dolorem!
+        .column.is-3
+          .s_item
+            img(src="../../src/assets/img/latestposts/banner.png", alt="")
+            h1 Title
+            p  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident accusantium, ad sunt dignissimos sit nisi harum assumenda repellendus ipsam dolore sequi voluptas delectus placeat optio tempore quod voluptatibus est dolorem!
+        .column.is-3
+          .s_item
+            img(src="../../src/assets/img/latestposts/banner.png", alt="")
+            h1 Title
+            p  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident accusantium, ad sunt dignissimos sit nisi harum assumenda repellendus ipsam dolore sequi voluptas delectus placeat optio tempore quod voluptatibus est dolorem!
 </template>
 
 <style lang="sass" scoped>
+
+section
+  text-align: center
+  background-color: #f6f6f6
+  .container
+    h1.s_title
+      padding-top: 100px
+      padding-bottom: 60px
+      font-size: 16px
+      text-transform: uppercase
+    .s_item
+      margin: 24px
+      background-color: #fff
+      border-radius: 8px
+      box-shadow: 0 0 7px rgba(0,0,0,0.5)
+      img
+        border-top-right-radius: 4px
+        border-top-left-radius: 4px
+      h1
+        padding: 10px
+        padding-bottom: 0
+      p
+        padding: 20px
+        padding-top: 15px
+        font-size: 14px
 
 </style>
